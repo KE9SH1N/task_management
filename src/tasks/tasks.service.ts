b0600583cd622e2e65dbbd880a497771aca59528
ke9sh1n <k3nshi9@gmail.com>
2025-01-08 14:20:41 +0600

    added auth guard and entity relationship

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,14 +7,15 @@ import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
   constructor(private readonly tasksRepository: TasksRepository) {}
 
   // private tasks: Task[] = [];
-  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
-    return this.tasksRepository.getTasks(filterDto);
+  getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto, user);
   }
   // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
   //   const { status, search } = filterDto;
@@ -36,18 +37,18 @@ export class TasksService {
   //   return tasks;
   // }
 
-  async getTaskById(id: string): Promise<Task> {
-    return this.tasksRepository.getTaskById(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    return this.tasksRepository.getTaskById(id, user);
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.tasksRepository.createTask(createTaskDto);
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto, user);
   }
 
   async deleteTask(id: string): Promise<void> {
     return this.tasksRepository.deleteTask(id);
   }
-  async updateTaskStatus(id: string, status: TaskStatus) {
-    return this.tasksRepository.updateTaskStatus(id, status);
+  async updateTaskStatus(id: string, status: TaskStatus, user: User) {
+    return this.tasksRepository.updateTaskStatus(id, status, user);
   }
 }
